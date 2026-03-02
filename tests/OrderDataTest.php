35ecba29f5fb0edfaf175bc8602ca3422100ed84
poncho213423424214 <hqi35203@laoia.com>
2026-03-02 16:34:33 +0700

    Модульные тесты phpunit

---
+++ tests/OrderDataTest.php
@@ -0,0 +1,23 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Services\ValidateOrderData;
+
+class OrderDataTest extends TestCase 
+{
+    private array $data;
+    private ValidateOrderData $obj;
+
+    public function setUp():void {
+        $this->data = [];
+        $this->data['fio'] = "Иванов";
+        $this->data['address'] = "Кемерово, ул.Тухачевского 32";
+        $this->data['phone'] = "89007009911";
+        $this->data['email'] = "ivanov@example.com";
+        $this->obj = new ValidateOrderData();
+    }
+
+    public function testValidateOrderData(): void {
+        $this->assertSame( true, 
+                           $this->obj->validate($this->data) );
+    }
+}
