aa406c20169390ba670f3f8e0bd76126f012d422
unknown <student@KCT-409-05.coopteh.local>
2026-03-03 16:15:32 +0700

    Final control work on OOP

---
+++ src/Model/Timestampable.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Model;
+
+trait Timestampable {
+    private string $createdAt;
+    private string $updatedAt;
+
+    public function setTimestamps(): void {
+        $this->createdAt = date('Y-m-d H:i:s');
+        $this->updatedAt = date('Y-m-d H:i:s');
+    }
+
+    public function updateTimestamp(): void {
+        $this->updatedAt = date('Y-m-d H:i:s');
+    }
+
+    public function getTimestamps(): array {
+        return [
+            'createdAt' => $this->createdAt ?? 'N/A',
+            'updatedAt' => $this->updatedAt ?? 'N/A',
+        ];
+    }
+}
