c4ccd620d6d77350943516338b18b70576fdc10b
joligw <sasho8293@gmail.com>
2026-03-05 11:25:09 +0700

    Создание первого issue (#1)
    исправил
    
    Создание первого issue #1

--- tests/OrderDataTest.php
+++ tests/OrderDataTest.php
@@ -21,7 +21,7 @@ class OrderDataTest extends TestCase {
         $this->assertSame( true, 
                            $this->obj->validate($this->data) );
     }
-        $this->assertfio( true, 
+        $this->assertfio( false, 
                            $this->obj->validate($this->data) );
     }
 }
