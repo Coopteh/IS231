86ee831ceb12f599b1b6be06893b0df7da5176b8
unknown <student@KCT-409-01.coopteh.local>
2026-03-23 17:17:43 +0700

    Главная страница с каруселью и описанием

--- src/Views/ProductTemplate.php
+++ src/Views/ProductTemplate.php
@@ -19,6 +19,10 @@ class ProductTemplate extends BaseTemplate {
                         <a href="/products/{$item['id']}"><h2>{$item['name']}</h2></a>
                         <p>{$item['description']}</p>
                         <h3>{$item['price']} ₽</h3>
+                         <form class="mt-4" action="/basket" method="POST">
+                            <input type="hidden" name="id" value="{$item['id']}">
+                            <button type="submit" class="btn btn-primary">Добавить в корзину</button>
+                        </form>
                     </div>
                 </div>
                 <hr>
@@ -43,6 +47,10 @@ class ProductTemplate extends BaseTemplate {
                         <h5 class="card-title">{$data['name']}</h5>
                         <p class="card-text">{$data['price']} рублей</p>
                         <p class="card-text"><small class="text-body-secondary">{$data['description']}</small></p>
+                        <form class="mt-4" action="/basket" method="POST">
+                            <input type="hidden" name="id" value="{$data['id']}">
+                            <button type="submit" class="btn btn-primary">Добавить в корзину</button>
+                        </form>
                     </div>
                     </div>
                 </div>
